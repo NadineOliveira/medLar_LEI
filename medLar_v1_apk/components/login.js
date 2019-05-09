af990ecb077274c0a194f37a3a554fecfc8c6f1d
anguinho <anguinho95@gmail.com>
2019-05-09 18:05:40 +0100

    Login done

--- medLar_v1_apk/components/login.js
+++ medLar_v1_apk/components/login.js
@@ -1,54 +1,79 @@
 import React, { Component } from "react";
-import { ScrollView, Text, TextInput, View, Button } from "react-native";
+import { AsyncStorage, ScrollView, Text, TextInput, View, Button } from "react-native";
 import { connect } from "react-redux";
 import axios from 'axios';
 
 class Login extends Component {
 
-  state = {
-        username: '',
-        password: '',
-        token: ''
-      };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+            username: '',
+            password: '',
+            token: ''
+          };
+          this.handleChangeUser = this.handleChangeUser.bind(this)
+          this.handleChangePass = this.handleChangePass.bind(this)
+          this.login = this.login.bind(this)
+  }
+
+  async storeItem(key, item) {
+    try {
+        let json = await AsyncStorage.setItem(key, JSON.stringify(item));
+        return json;
+    } catch (error) {
+        console.log(error.message);
+    }
+  }
+  
+  handleChangeUser(event = {}) {
+    this.setState({username: event.nativeEvent.text})
+  }
+
+
+  handleChangePass(event = {}) {
+    this.setState({password: event.nativeEvent.text})
+  }
+
+
+  login() {
+        console.warn(this.state)
+
+        axios.post('http://192.168.1.210:8000/login',{
+                    id: this.state.username,
+                    password: this.state.password
+                }).then(function(res) {
+                    console.warn(res.data.token);
+                }).catch(function(e) {console.warn(e)})
+   };
+
+
 
   render() {
     return (
       <ScrollView style={{ padding: 20 }}>
         <Text style={{ fontSize: 27 }}>Login</Text>
         <TextInput
+            id = "username"
             placeholder="Username"
-            on
-            onChangeText={(text) => this.setState({ username: text })}
+            /*onChangeText={(text) => this.setState({ username: text.value })}*/
+            onChange={this.handleChangeUser}
             value={this.state.username}
         />
         <TextInput
+            id = "password"
             placeholder="Password"
             secureTextEntry={true}
-            onChangeText={(text) => this.setState({ password: text })}
+            /*onChangeText={(text) => this.setState({ password: text.value })}*/
+            onChange={this.handleChangePass}
             value={this.state.password}
 
         />
         <View style={{ margin: 7 }} />
 
         <Button
-          onPress={() => {
-                console.warn(this.state.username);
-                console.warn(this.state.password);
-                axios.post('http://localhost:8000/login', {
-                    id: this.state.username,
-                    password: this.state.password
-                }).then(res => {
-                    this.state.token = res.data.token;
-                    axios.create({
-                      baseURL: 'https://localhost:8000/api/',
-                      timeout: 1000,
-                      headers: {'authorization': 'Bearer '+res.data.token}
-                    });
-                    this.props.navigate.navigate("Home")
-
-                }).catch(e => alert.show('Login Incorrect'))
-            }
-          }
+          onPress={this.login}
           title="SignIn"
         />
         <View style={{ margin: 10 }} />
@@ -58,9 +83,4 @@ class Login extends Component {
   }
 }
 
-const mapStateToProps = state => {
-  const { login } = state;
-  return { login };
-};
-
-export default connect(mapStateToProps)(Login);
+export default connect()(Login);
